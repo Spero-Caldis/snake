beb1075bf15a0a99908ffa8a4072876ef026fa2d
spcaldicpt024 <spcaldicpt024@student.wethinkcode.co.za>
2024-11-01 14:31:26 +0200

    added agent.py and wrote Agent class

---
+++ constants.py
@@ -0,0 +1,16 @@
+
+# agent constants (machine learning stuff)
+MAX_MEMORY = 100_000
+BATCH_SIZE = 1000
+LR = 0.001
+
+# rgb colours
+WHITE = (255, 255, 255)
+RED = (200,0,0)
+BLUE1 = (0, 0, 255)
+BLUE2 = (0, 100, 255)
+BLACK = (0,0,0)
+
+# snake constants
+BLOCK_SIZE = 20
+SPEED = 20
